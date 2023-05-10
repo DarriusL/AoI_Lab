c35a5ba84aacd45cf39b58472a3a95b01b77f476
DarriusL <darrius.lei@outlook>
2023-05-10 19:22:52 +0800

    update

--- lib/util/cdtstm_util.m
+++ lib/util/cdtstm_util.m
@@ -3,31 +3,30 @@ function stc = cdtstm_util
     stc.mc_switch = @mc_switch;
 end
 
-%实用函数信息
+%utility function information
 function info
-    disp(['条件判断相关实用函数(cdtstm_util)', newline, ...
+    disp(['Conditional Judgment Related Utility Functions(cdtstm_util)', newline, ...
         '----------------------------------------------', newline, ...
-        '1.mc_switch 多条件switch及其配置', newline, ...
-        '----------------------------------------------', newline, ...
-        '最后一次更新：2022-08-29']);
+        '1.mc_switch Multi-condition switch and its configuration', newline, ...
+        '----------------------------------------------', newline, ]);
 end
 
-%多条件switch
+%Multi-condition switch
 function stc = mc_switch
     stc.config = @mc_switch_config;
     stc.mulcon_switch = @mulcon_switch;
 end
 
-%------------------------相关函数---------------------------
+%------------------------related functions---------------------------
 function stc = mc_switch_config
-    %多条件switch配置
+    %Multi-condition switch configuration
     stc.con_num = 2;
     stc.fun = cell(2, 1);
     stc.fun_con = {cell(2,1), cell(2,1)};
 end
 
 function vcell = mulcon_switch(cdt, config)
-    %多条件switch执行
+    %Multi-condition switch execution
     vcell = cell(config.con_num, 1);
     assert(length(config.fun) == config.con_num && length(config.fun_con) == config.con_num);
     for i = 1:config.con_num
